6f598d266860d763645171c8d60105a79fa0c36c
남재현 <nam2350@gmail.com>
2021-08-18 17:37:39 +0900

    카카오로그인 라이브러리 삭제

--- src/Component/Jaehyun/LibraryTest.js
+++ src/Component/Jaehyun/LibraryTest.js
@@ -3,21 +3,25 @@ import DaumPostcode from "react-daum-postcode";
 
 const LibraryTest = () => {
   const handleComplete = (data) => {
-    let fullAddress = data.fullAddress;
+    console.log("주소데이타 : ", data);
+    let fullAddress = "";
     let extraAddress = "";
 
     if (data.addressType === "R") {
       if (data.bname !== "") {
         extraAddress += data.bname;
+        // console.log("비네임", extraAddress);
       }
       if (data.buildingName !== "") {
         extraAddress +=
           extraAddress !== "" ? `, ${data.buildName}` : data.buildingName;
+        // console.log("빌딩네임", extraAddress);
       }
       fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
     }
 
-    console.log(fullAddress);
+    // console.log("검색완료!", fullAddress);
+    // console.log("검색완료!", extraAddress);
   };
   return <DaumPostcode onComplete={handleComplete} />;
 };
