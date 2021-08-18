36aabb1f41244bd4f995b0e410b62ddf63210954
name2350 <name2350@gmail.com>
2021-08-18 09:53:02 +0900

    라이브러리 테스트

--- src/Component/Jaehyun/LibraryTest.js
+++ src/Component/Jaehyun/LibraryTest.js
@@ -1,7 +1,25 @@
 import React from "react";
+import DaumPostcode from "react-daum-postcode";
 
 const LibraryTest = () => {
-  return <div></div>;
+  const handleComplete = (data) => {
+    let fullAddress = data.fullAddress;
+    let extraAddress = "";
+
+    if (data.addressType === "R") {
+      if (data.bname !== "") {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== "") {
+        extraAddress +=
+          extraAddress !== "" ? `, ${data.buildName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
+    }
+
+    console.log(fullAddress);
+  };
+  return <DaumPostcode onComplete={handleComplete} />;
 };
 
 export default LibraryTest;
--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import LoginJH from "./Component/Jaehyun/LoginJH";
+import App from "./App";
+import LoginJH from "./Component/Jaehyun/Login/LoginJH";
 import KakaoLogin from "./Component/Jungmin/Login/KakaoLogin";
+import LibraryTest from "./Component/Jaehyun/LibraryTest";
 
 function Routes() {
   return (
     <Router>
       <Switch>
+        <Route exact path="/" component={App} />
         <Route exact path="/loginjh" component={LoginJH} />
         <Route exact path="/kakaologin" component={KakaoLogin} />
+        <Route exact path="/postcode" component={LibraryTest} />
       </Switch>
     </Router>
   );
